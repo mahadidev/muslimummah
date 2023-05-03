2b2b4a110c36dd0176ca1ce7ffae7bb5cddb3be2
mahadidev <mahadi.dev.pm@gmail.com>
2023-05-03 07:35:10 +0600

    Test Routing

---
+++ src/pages/shop.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const ShopPage = () => {
+  return (
+    <div className="w-full h-screen flex items-center justify-center">
+      <div>
+        <h1 className="text-2xl">Shop Page</h1>
+        <a className="underline text-center mx-auto" href="/">
+          Home
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default ShopPage;
