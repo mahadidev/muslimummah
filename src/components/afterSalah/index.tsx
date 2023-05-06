250f17918da09c9263a19a5f785ee1c89e35e9b9
mahadidev <mahadi.dev.pm@gmail.com>
2023-05-06 10:23:19 +0600

    update problem solution duya

--- src/components/afterSalah/index.tsx
+++ src/components/afterSalah/index.tsx
@@ -85,6 +85,24 @@ const AfterSalah = () => {
             ))}
           </div>
         </div>
+        <a
+          href="#"
+          className="w-full inline-flex justify-center items-center mt-2 py-2 sm:py-3 px-2 sm:px-5 text-sm sm:text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300"
+        >
+          আরো পরুন
+          <svg
+            className="ml-2 -mr-1 w-5 h-5"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+              clip-rule="evenodd"
+            ></path>
+          </svg>
+        </a>
       </div>
     </section>
   );
